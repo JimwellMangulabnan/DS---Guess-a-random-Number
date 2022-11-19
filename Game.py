82255e37a6058303ebe14147e12c9bf89febd616
JimwellMangulabnan <mangulabnanjimwell22@gmail.com>
2022-11-19 20:12:02 +0800

    user input must be larger than 0 and a number

--- Game.py
+++ Game.py
@@ -4,4 +4,12 @@ top_of_range = input("Type a number: ")
 
 if top_of_range.isdigit():
     top_of_range = int(top_of_range)
+
+    if top_of_range <= 0:
+        print("Please type a number larger than 0 next time! ")
+        quit()
+else: 
+    print("please type a Number next time! ")
+    quit()
+
     
